52e32037e37de80f442ba6423960fc306758a517
laserzz <82984448+laserzz@users.noreply.github.com>
2023-03-09 19:44:17 -0800

    help cmd (stole from my other bot shh)

---
+++ dist/commands/help.js
@@ -0,0 +1,90 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.command = exports.run = void 0;
+const discord_js_1 = require("discord.js");
+async function run(interaction, bot) {
+    const commands = bot.collectCommands();
+    let cmd = interaction.options.getString('command');
+    if (cmd) {
+        if (cmd.includes(' ')) {
+            let args = cmd.split(' ');
+            let cmdres = commands.find(c => c.name == args[0].toLowerCase());
+            if (!cmdres) {
+                await interaction.followUp({ content: `command ${args[0]} not found!` });
+                return;
+            }
+            if (cmdres.options[0] && cmdres.options[0].type != 1) {
+                await interaction.followUp({ content: `command ${args[0]} has no subcommands!` });
+                return;
+            }
+            let subcmd = cmdres.options.filter(c => c.name == args[1].toLowerCase())[0];
+            if (!subcmd) {
+                await interaction.followUp({ content: `subcommand ${args[1]} not found!` });
+                return;
+            }
+            let descStr = "";
+            if (!subcmd.options) {
+                descStr += "No Options";
+            }
+            else {
+                for (const o of subcmd.options) {
+                    descStr += `**${o.name}** - ${o.description}\n`;
+                }
+            }
+            let embed = {
+                title: `Command Info For ${cmd}`,
+                description: descStr,
+                color: 0x000099
+            };
+            await interaction.followUp({ embeds: [embed] });
+        }
+        else {
+            let cmdres = commands.find(c => c.name == cmd.toLowerCase());
+            if (!cmdres) {
+                await interaction.followUp({ content: `command ${cmd} not found!` });
+                return;
+            }
+            let descStr = "";
+            if (!cmdres.options) {
+                descStr += "No Options";
+            }
+            else {
+                for (const o of cmdres.options) {
+                    descStr += `**${o.name}** - ${o.description}\n`;
+                }
+            }
+            let embed = {
+                title: `Command Info For ${cmd}`,
+                description: descStr,
+                color: 0x000099
+            };
+            await interaction.followUp({ embeds: [embed] });
+        }
+    }
+    else {
+        let descStr = "";
+        for (const c of commands) {
+            descStr += `**${c.name}** - ${c.description}\n`;
+        }
+        let embed = {
+            title: `Command Info`,
+            description: descStr,
+            color: 0x000099
+        };
+        await interaction.followUp({ embeds: [embed] });
+    }
+}
+exports.run = run;
+exports.command = {
+    name: 'help',
+    description: 'help command!',
+    type: discord_js_1.ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'command',
+            description: 'command to see info on',
+            type: discord_js_1.ApplicationCommandOptionType.String,
+            required: false
+        }
+    ]
+};
