4d17ed63c7974fac3205a1c91f3da9c7fcbca364
laserzz <82984448+laserzz@users.noreply.github.com>
2023-03-19 15:21:01 -0700

    lb cooldown

--- dist/commands/levels/sub/leaderboard.js
+++ dist/commands/levels/sub/leaderboard.js
@@ -1,7 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.leaderboard = void 0;
+const discord_js_1 = require("discord.js");
 const leaderboard = async (interaction, bot) => {
+    const cooldown = bot.getCooldown(interaction.user.id, 'leaderboard', 180000);
+    if (!cooldown[0]) {
+        const remainder = cooldown[1];
+        const embed = new discord_js_1.EmbedBuilder()
+            .setTitle("Slow Down")
+            .setDescription(`You must wait ${remainder} more minutes!`)
+            .setColor("Red")
+            .setTimestamp(new Date());
+        return embed;
+    }
     const users = await bot.getTotalXP(interaction.guildId);
     const embed = bot.doLevelLB(users, interaction.guildId);
     return embed;
--- src/commands/levels/sub/leaderboard.ts
+++ src/commands/levels/sub/leaderboard.ts
@@ -1,7 +1,18 @@
-import { ChatInputCommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import { MyClient } from "../../../bot";
 
 export const leaderboard = async (interaction: ChatInputCommandInteraction, bot: MyClient) => {
+    const cooldown = bot.getCooldown(interaction.user.id, 'leaderboard', 180000);
+    if(!cooldown[0]) {
+        const remainder = cooldown[1];
+        const embed = new EmbedBuilder()
+            .setTitle("Slow Down")
+            .setDescription(`You must wait ${remainder} more minutes!`)
+            .setColor("Red")
+            .setTimestamp(new Date());
+
+        return embed;
+    }
     const users = await bot.getTotalXP(interaction.guildId);
     const embed = bot.doLevelLB(users, interaction.guildId);
     return embed;
